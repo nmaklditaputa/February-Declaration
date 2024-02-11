e045188ad476b76417f245ca723d035e092b531f
nmaklditaputa <140451322+nmaklditaputa@users.noreply.github.com>
2024-02-11 03:12:03 -0400

    Update changeElements.js

--- src/hooks/changeElements.js
+++ src/hooks/changeElements.js
@@ -3,10 +3,10 @@ import confetti from 'canvas-confetti'
 
 export const useChangeElements = () => {
     const [index, setIndex] = useState(0)
-    const [textP, setTextP] = useState('Vamos, respondeme! ')
+    const [textP, setTextP] = useState('dale amor, no seas odiosa ')
     const [image, setImage] = useState('https://i.pinimg.com/originals/f6/a5/f7/f6a5f7ddff1f05cbcc560256b9f98c2e.gif')
 
-    const options = ['Segura?', 'Segurisima?', 'Estas completamente segura?', 'No te arrepentiras?', 'Pero si estas segura?', 'No hay vuelta atras', 'No hay devoluciones', 'No hay garantias', 'No hay reembolsos', 'No hay cambios', 'No hay nada', 'No hay', 'Que no hay']
+    const options = ['Segura?', 'Segurisima?', 'Ah ya, como ya le dijiste que sí al otro u otra', 'Tas segura de que no?', 'Todas iguales', 'DALE AMOOOR QUE TE CUESTA MARRR', 'No me hables, de pana', 'No que me amabas?', 'Ultima oportunidad para darle que sí', 'me voy a ir a llorar', 'ya sé que hacer para que le de si', 'Si le das que no, eres swiftie', 'Te voy a acusar con Kanye']
 
     const handleButtonNo = () => {
         setIndex(index + 1)
@@ -18,7 +18,7 @@ export const useChangeElements = () => {
 
     const handleButtonYes = () => {
         setImage("https://i.pinimg.com/originals/e4/9d/7b/e49d7b7e965f09e31b498314b02e3662.gif")
-        setTextP('Gracias por aceptar, te amo mucho <3')
+        setTextP('Yo sabía que ibas a aceptar, te amo mucho mi pedacito c:')
         confetti()
     }
 
